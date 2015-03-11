2cc2ec3fe5cce58308a9f4d0b969014f327799d4
geofholbrook <geof.holbrook@gmail.com>
2015-03-11 01:25:48 +0100

    wind tracks with background

--- wind.js
+++ wind.js
@@ -3,19 +3,29 @@ var windImage = new Image();
 windImage.src = "lib/wind-arrow.png";
 var windPatt = null;
 
+var windAngle = 0.2;
 
-
+var windCounter = 0;
+var windSpeed = 3;
 
 function drawWind(ctx)
 {
 	ctx.save();
-	ctx.globalAlpha = 0.2;
+	ctx.globalAlpha = 0.15;
 	ctx.fillStyle = windPatt;
 
-	// coords adjusted to correspond to canvas
-    ctx.fillRect(-can.width/2, -can.height/2, can.width, can.height);
+	windCounter = (windCounter + windSpeed) % 75;
+
+	ctx.rotate(windAngle);
+
+	var offset = { x: modulo( -boat.pos.x, windImage.width ),
+				   y: modulo(  boat.pos.y, windImage.height )  };		
+
+	ctx.translate(windCounter + offset.x, offset.y);
+	
+	// made double size so that it fills the screen
+    ctx.fillRect(-can.width, -can.height, can.width * 2, can.height * 2);
 
     ctx.globalAlpha = 1;
     ctx.restore();
-
 }
